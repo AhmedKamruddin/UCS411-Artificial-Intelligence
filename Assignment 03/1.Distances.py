2099162adfbde902ee95fb225ca70f586a74aa2d
AhmedKamruddin <98866395+AhmedKamruddin@users.noreply.github.com>
2022-03-24 21:11:27 +0530

    Update 1.Distances.py

--- Assignment 03/1.Distances.py
+++ Assignment 03/1.Distances.py
@@ -1,97 +1,75 @@
 from cmath import sqrt
 
-class MyEightPuzzle:
-    def __init__(self, startState, goalState, startState2D, goalState2D):
-        self.startState=startState
-        self.goalState=goalState
-        self.startState2D=startState2D
-        self.goalState2D=goalState2D
+class Heuristic:
+    def __init__(self, start, goal):
+        self.startState=start
+        self.goalState=goal
 
-    def euclidean(self):
-        sum=0
-        for i in range(0, 3):
-            for j in range(0, 3):
-                goalNode=self.goalState2D[i][j]
-                if goalNode==0:
-                    continue
-                goalIndexX=i
-                goalIndexY=j
-
-                for k in range(0, 3):
-                    for l in range(0, 3):
-                        startNode=self.startState2D[k][l]
-                        if startNode==goalNode:
-                            startIndexX=k
-                            startIndexY=l
-                            break
-                
-                x=abs(goalIndexX-startIndexX)
-                y=abs(goalIndexY-startIndexY)
-                distance=sqrt( pow(x, 2) + pow(y, 2) )
-                sum+=distance
-                print(f"Euclidean distance for {goalNode} is {distance}")
-        print(f"Heuristic function value for Euclidean distance={sum}")
-        
-    def manhattan(self):
+    def Euclidean(self):
         sum=0
         for i in range(0, 9):
-            
-            goalNode=self.goalState[i]
-            if goalNode==0:
-                continue
-            goalIndex=i
+            if self.goalState[i]!=0:
+                goalBlockX=int(i/3)
+                goalBlockY=i%3     
 
-            for j in range(0, 9):
-                startNode=self.startState[j]
-                if startNode==goalNode:
-                    startIndex=j
-                    break
-            
-            difference=abs(goalIndex-startIndex)
-            if difference<3:
-                moves=difference
-            elif difference>=3 and difference<6:
-                moves=difference%3 + 1
-            elif difference>=6 and difference<8:
-                moves=difference%3 + 2
-            sum+=moves
-            print(f"Manhattan distance for {self.goalState[i]} is {moves}")
-        print(f"Heuristic function value for Manhattan distance={sum}")
-            
-    def minkowski(self):
-        sum=0
-        p=float(input("Enter p: "))
-
-        for i in range(0, 3):
-            for j in range(0, 3):
-                goalNode=self.goalState2D[i][j]
-                if goalNode==0:
-                    continue
-                goalIndexX=i
-                goalIndexY=j
-
-                for k in range(0, 3):
-                    for l in range(0, 3):
-                        startNode=self.startState2D[k][l]
-                        if startNode==goalNode:
-                            startIndexX=k
-                            startIndexY=l
-                            break
-                
-                x=abs(goalIndexX-startIndexX)
-                y=abs(goalIndexY-startIndexY)
-                distance=pow( pow(x, p) + pow(y, p), 1/p )
+                for j in range(0, 9):
+                    if self.goalState[i]==self.startState[j]:
+                        startBlockX=int(j/3)
+                        startBlockY=j%3
+                        break
+                distance=sqrt( pow(goalBlockX-startBlockX, 2) + pow(goalBlockY-startBlockY, 2) )
+                print(f"Euclidean distance for {self.goalState[i]} is {distance}")
                 sum+=distance
-                print(f"Minkowski distance for {goalNode} is {distance}")
-        print(f"Heuristic function value for Minkowski distance={sum}")
         
+        print(f"Heuristic function value for Euclidean distance={sum}")
+
+    def Manhattan(self):
+        sum=0
+
+        for i in range(0, 9):
+            if self.goalState[i]!=0:
+                goalBlockIndex=i
+                for j in range(0, 9):
+                    if self.goalState[i]==self.startState[j]:
+                        startBlockIndex=j
+                        break
+                
+                difference=abs(goalBlockIndex-startBlockIndex)
+                if difference in [0, 1, 2]:
+                    moves=difference
+                elif difference in [3, 4, 5]:
+                    moves=difference%3+1
+                elif difference in [6, 7, 8]:
+                    moves=difference%3+2
+                
+                sum+=moves
+                print(f"Manhattan distance for {self.goalState[i]} is {moves}")
+        print(f"Heuristic function value for Manhattan distance={sum}")
+
+    def Minkowski(self):
+        p=float(input("Enter value of p: "))
+        sum=0
+
+        for i in range(0, 9):
+            if self.goalState[i]!=0:
+                goalBlockX=int(i/3)
+                goalBlockY=i%3     
+
+                for j in range(0, 9):
+                    if self.goalState[i]==self.startState[j]:
+                        startBlockX=int(j/3)
+                        startBlockY=j%3
+                        break
+                distance=pow( pow(goalBlockX-startBlockX, p) + pow(goalBlockY-startBlockY, p) , 1/p)
+                print(f"Minkowski distance for {self.goalState[i]} is {distance}")
+                sum+=distance
+        
+        print(f"Heuristic function value for Minkowski distance={sum}")
+                
 
 start=[2, 0, 3, 1, 8, 4, 7, 6, 5]
 goal= [1, 2, 3, 8, 0, 4, 7, 6, 5]
-start2D=[[2, 0, 3], [1, 8, 4], [7, 6, 5]]
-goal2D= [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
-problem=MyEightPuzzle(start, goal, start2D, goal2D)
-
-problem.euclidean()
-problem.manhattan()
-problem.minkowski()
+problem=Heuristic(start, goal)
+problem.Euclidean()
+problem.Manhattan()
+problem.Minkowski()
