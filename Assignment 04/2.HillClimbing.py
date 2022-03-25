4d0e8ad5224f1472d69069cbc2b442e6baf328f8
AhmedKamruddin <98866395+AhmedKamruddin@users.noreply.github.com>
2022-03-25 09:44:22 +0530

    Update 2.HillClimbing.py

--- Assignment 04/2.HillClimbing.py
+++ Assignment 04/2.HillClimbing.py
@@ -7,10 +7,7 @@ class MyBlockProblem:
         self.prevState=None
 
     def isGoalReached(self):
-        #print("In isGoalReached()")
         for i in range(0, 4):
-            #print("Printing self.currentState[i]")
-            #print(self.currentState[i])
             if self.currentState[i]==goal:
                 return True
         
@@ -24,9 +21,6 @@ class MyBlockProblem:
                 print("------------------")
         print("******************************************")
 
-    def _eq_(self, other):
-        return self.currentState==other.currentState
-
     def movefromStackXtoStackY(self, x, y):
         if self.currentState[x]!=[] and len(self.currentState[y])!=4:
             self.prevState=copy.deepcopy(self)
@@ -37,15 +31,12 @@ class MyBlockProblem:
             return False
 
     def possibleNextStates(self):
-        #print("Over here")
         stateList=[]
         for i in range(0, 4):
             for j in range(0, 4):
                 copy_state=copy.deepcopy(self)
                 if i!=j and copy_state.movefromStackXtoStackY(i, j):
-                    #copy_state.displayState()
                     stateList.append(copy_state)
-                    #print("Appending to stateList ")
                     
         return stateList
 
@@ -71,15 +62,10 @@ class MyBlockProblem:
                     break
             
             if currentBlockIndexY!=goalBlockIndex:
-                #print(f"-{currentBlockIndexY}({self.currentState[currentBlockIndexX][currentBlockIndexY]})")
                 value-=currentBlockIndexY
             else:
-                #print(f"+{currentBlockIndexY}({self.currentState[currentBlockIndexX][currentBlockIndexY]})")  
-                #print(f"{value}={value}+{currentBlockIndexY}")
                 value+=currentBlockIndexY
 
-
-                
         return value
 
 def constructPath(goalState):
@@ -94,35 +80,27 @@ def HillClimbing(startState):
     open=[]
     closed=[]
     
-    #Step 1
     open.append(startState)
 
-
-    #Step 2
     returnVal=0
     while open:
 
-        #
         thisState=open.pop(0)
-        #print("Printing thisState")
         thisState.displayState()
 
-        #Step 4
         if thisState.isGoalReached():
             print("Goal state found.. stopping search")
             returnVal=constructPath(thisState)
             break
 
-        #Step 5
         nextStates=thisState.possibleNextStates()
 
-        #Step 6
         for eachState in nextStates:
             if eachState not in open and eachState not in closed:
-                #If next state is better than current state(higher heuristic value is better)
                 if eachState.heuristic() > thisState.heuristic():
                     open.append(eachState)
                     closed.append(thisState)
+                    break
     
     if returnVal!=1:
         print("Error: Local Maxima")
@@ -131,5 +109,4 @@ def HillClimbing(startState):
 start=[[2, 3, 4, 1], [], [], []]
 goal=[1, 2, 3, 4]
 problem=MyBlockProblem(start, goal)
-#print(problem.heuristic())
 HillClimbing(problem)
